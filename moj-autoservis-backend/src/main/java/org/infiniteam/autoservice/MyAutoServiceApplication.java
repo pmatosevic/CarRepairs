4ae340fdbcc0732a611dfa82649d06db671ff0f0
Patrik Matošević <patrik.matosevic@fer.hr>
2019-10-15 21:40:46 +0200

    Create Spring Boot maven project for backend

---
+++ moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/MyAutoServiceApplication.java
@@ -0,0 +1,13 @@
+package org.infiniteam.autoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAutoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAutoServiceApplication.class, args);
+	}
+
+}
