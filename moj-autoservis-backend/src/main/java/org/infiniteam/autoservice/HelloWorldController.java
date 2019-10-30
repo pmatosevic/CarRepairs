b38cdb5db673b6e1123f80400fccdc7a0debea30
Patrik Matošević <patrik.matosevic@fer.hr>
2019-10-30 10:20:47 +0100

    Include Thymeleaf and Dev tools dependency

--- moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/HelloWorldController.java
+++ moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/HelloWorldController.java
@@ -1,14 +1,18 @@
 package org.infiniteam.autoservice;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello world!";
+    public String hello(Model model) {
+        model.addAttribute("message", "world");
+
+        return "hello";
     }
 
 }
