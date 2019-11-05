4e315ed75b01938b0c2bd2b1378ef0b30309099a
Patrik Matošević <patrik.matosevic@fer.hr>
2019-11-05 23:11:20 +0100

    Update Spring Security config

--- moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/WebSecurity.java
+++ moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/WebSecurity.java
@@ -1,19 +1,56 @@
 package org.infiniteam.autoservice;
 
+import org.infiniteam.autoservice.model.Administrator;
+import org.infiniteam.autoservice.model.ServiceEmployee;
+import org.infiniteam.autoservice.model.User;
+import org.infiniteam.autoservice.model.VehicleOwner;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private Map<Class<? extends User>, List<GrantedAuthority>> authorities = Map.of(
+            VehicleOwner.class, AuthorityUtils.createAuthorityList("ROLE_USER"),
+            ServiceEmployee.class, AuthorityUtils.createAuthorityList("ROLE_SERVICE_EMPLOYEE"),
+            Administrator.class, AuthorityUtils.createAuthorityList("ROLE_ADMINISTRATOR")
+    );
+
+    private Map<Class<?>, String> redirects = Map.of(
+            VehicleOwner.class, "/user",
+            ServiceEmployee.class, "/autoservice",
+            Administrator.class, "/admin"
+    );
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().loginPage("/login").permitAll();
-        http.logout().logoutUrl("/logout");
-        http.csrf().disable();
+        http.formLogin()
+                .loginPage("/login")
+                .successHandler((req, resp, auth) -> resp.sendRedirect(redirects.get(auth.getPrincipal().getClass())))
+                .failureForwardUrl("/login?error=true");
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/");
         http.headers().frameOptions().sameOrigin();
 //        http.authorizeRequests().anyRequest().authenticated();
         http.authorizeRequests().anyRequest().permitAll();
     }
+
+
+
 }
