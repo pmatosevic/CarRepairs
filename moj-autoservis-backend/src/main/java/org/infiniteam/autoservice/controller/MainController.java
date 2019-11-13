2ba41430176d233945364e9fa7548e865948e7f5
Patrik Matošević <patrik.matosevic@fer.hr>
2019-11-13 21:34:24 +0100

    Fix user registration

--- moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/controller/MainController.java
+++ moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/controller/MainController.java
@@ -5,13 +5,17 @@ import org.infiniteam.autoservice.repository.AutoServiceRepository;
 import org.infiniteam.autoservice.repository.UserRepository;
 import org.infiniteam.autoservice.security.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 import java.security.Principal;
 import java.util.Map;
 
@@ -26,7 +30,12 @@ public class MainController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
     private AutoServiceRepository autoServiceRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/")
     public String index(Model model, Principal principal) {
@@ -43,37 +52,29 @@ public class MainController {
         return "login";
     }
 
-    @Secured("ROLE_USER")
-    @GetMapping("/user")
-    public String carOwnerHome(Model model) {
-        return "user/home";
-    }
-
-    @Secured("ROLE_SERVICE_EMPLOYEE")
-    @GetMapping("/autoservice")
-    public String autoserviceHome(Model model) {
-        return "autoservice/home";
-    }
-
-    @Secured("ROLE_ADMIN")
-    @GetMapping("/admin")
-    public String adminHome(Model model) {
-        return "admin/home";
-    }
-
     @GetMapping("/register/user")
     public String registrationFormUser(Model model){
         return "user/registration";
     }
 
     @PostMapping("/register/user")
-    public String userRegistration(HttpServletRequest request, Model model) {
-        AppUser newUser = new VehicleOwner();
-        newUser.setUsername(request.getParameter("username"));
+    @Transactional
+    public ResponseEntity<String> userRegistration(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (userRepository.getByUsername(username).isPresent()) {
+            return new ResponseEntity<>("Username already registered.", HttpStatus.BAD_REQUEST);
+        }
+
+        VehicleOwner newUser = new VehicleOwner();
+        newUser.setUsername(username);
         newUser.setEmail(request.getParameter("email"));
-        newUser.setPasswordHash(request.getParameter("password"));
-        userRepository.save(newUser);
-        return "redirect:/login";
+        newUser.setOib(request.getParameter("oib"));
+        newUser.setFirstName(request.getParameter("name"));
+        newUser.setLastName(request.getParameter("surname"));
+        newUser.setPasswordHash(passwordEncoder.encode(password));
+        userRepository.saveAndFlush(newUser);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/register/autoservice")
@@ -83,7 +84,6 @@ public class MainController {
 
     @PostMapping("/register/autoservice")
     public String autoServiceRegistration(HttpServletRequest request, Model model) {
-
         AutoService newAutoService = new AutoService();
         newAutoService.setShopName(request.getParameter("shopname"));
         newAutoService.setAddress(request.getParameter("address"));
