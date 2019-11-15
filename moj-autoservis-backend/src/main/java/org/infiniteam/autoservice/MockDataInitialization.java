a5e2fa556fc87139c071bf3fe22cc5108440b14a
Patrik Matošević <patrik.matosevic@fer.hr>
2019-11-15 18:26:11 +0100

    Update class diagram

--- moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/MockDataInitialization.java
+++ moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/MockDataInitialization.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Initialization {
+public class MockDataInitialization {
 
     @Autowired
     private UserRepository userRepository;
