d70eec323c1856d54ed8430873e77c935cfa540a
Patrik Matošević <patrik.matosevic@fer.hr>
2020-01-07 11:54:01 +0100

    Add basic home page for vehicle owner

--- moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/controller/VehicleOwnerController.java
+++ moj-autoservis-backend/src/main/java/org/infiniteam/autoservice/controller/VehicleOwnerController.java
@@ -1,17 +1,24 @@
 package org.infiniteam.autoservice.controller;
 
+import org.infiniteam.autoservice.model.Vehicle;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Secured("ROLE_USER")
 public class VehicleOwnerController {
 
-    @Secured("ROLE_USER")
     @GetMapping("/user")
     public String carOwnerHome(Model model) {
         return "user/home";
+    }
+
+    @PostMapping("/user/vehicles")
+    public Vehicle addVehicle(Vehicle vehicleDto) {
+        return null;
     }
 
 }
